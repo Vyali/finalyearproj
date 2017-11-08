6e8efa312378e453376178b0ec13a6f08a59faba
Ayush Chaurasia <ayushchaurasia192@gmail.com>
2017-11-09 01:04:10 +0530

    Add files via upload

---
+++ Healthcare/main_app/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+# Create your models here.
+class Servicecard(models.Model):
+    name = models.CharField(max_length = 100)
+    description = models.CharField(max_length = 100)
+    def __str__(self):
+        return self.name
